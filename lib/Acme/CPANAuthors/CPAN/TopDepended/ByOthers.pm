c4ed153a16523d76ab5877ff7965640631556f4e
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-10-20 17:43:29 +0700

    Fix Synopsis

--- lib/Acme/CPANAuthors/CPAN/TopDepended/ByOthers.pm
+++ lib/Acme/CPANAuthors/CPAN/TopDepended/ByOthers.pm
@@ -16,7 +16,7 @@ use Acme::CPANAuthors::Register (
 =head1 SYNOPSIS
 
    use Acme::CPANAuthors;
-   use Acme::CPANAuthors::TopDepended::ByOthers;
+   use Acme::CPANAuthors::CPAN::TopDepended::ByOthers;
 
    my $authors = Acme::CPANAuthors->new('CPAN::TopDepended::ByOthers');
 
